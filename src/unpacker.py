6454b64412c3f9fba5518a3c22dc74e9392a69b9
jake <C92i747A@pm.me>
2023-12-17 17:35:24 +0000

    Strips only \n

--- src/unpacker.py
+++ src/unpacker.py
@@ -1,6 +1,6 @@
 def unpackCsv(csvFile: str): # This is gonna assume that the the headers of the table are vertical
     with open(csvFile, "r") as csvContents:
-        cleanedList = [line.strip() for line in csvContents]
+        cleanedList = [line.strip("\n") for line in csvContents]
         separatedLines = [line.split(",") for line in cleanedList]
         
         return separatedLines
