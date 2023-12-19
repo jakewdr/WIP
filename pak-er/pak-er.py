442e1edfe0b0db4951d52220e2e1ac971ed7488e
jake <C92i747A@pm.me>
2023-12-19 16:48:13 +0000

    Started porting the pak-er + adding the image compressor

---
+++ pak-er/pak-er.py
@@ -0,0 +1,28 @@
+import shutil
+import base64
+import os
+
+containingFolder = str(os.path.realpath(__file__).replace(os.sep, '/')).replace("/pak-er.py", "")
+
+def paker(folderToBePacked:str, outputLocation:str, manifestName:str):
+    with open(folderToBePacked + manifestName, "r") as cfgContents:
+        cleanedList = [line.strip("\n") for line in cfgContents]
+        separatedLines = [line.split("=") for line in cleanedList]
+
+    shutil.make_archive(separatedLines[0][1], "zip", folderToBePacked)
+    if os.path.isfile(separatedLines[0][1] + ".pak"): os.remove(separatedLines[0][1] + ".pak")
+    with open(separatedLines[0][1] + ".zip", "rb") as fileIn, open(separatedLines[0][1] + ".pak", "wb") as fileOut:
+        base64.encode(fileIn, fileOut)
+    if os.path.isfile(separatedLines[0][1] + ".zip") == True: os.remove(separatedLines[0][1] + ".zip")
+    
+def imageCompressor(imagepath:str):
+    pass
+
+if __name__ == "__main__":
+    PAKNAME = "template"
+    paker(containingFolder + f"/src/{PAKNAME}/", containingFolder +"/out/", "manifest.cfg")
+    
+    
+    
+    
+# I will finish this at some point
