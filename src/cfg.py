166574eb07f1cdfcc1a15dcac8dca91ecd819105
jake <C92i747A@pm.me>
2023-12-27 14:36:37 +0000

    Fixed/optimized config system

--- src/cfg.py
+++ src/cfg.py
@@ -1,14 +1,7 @@
-from typing import Dict, List
-from functools import cache
+from typing import Dict
 
 def unpackCfg(cfgFile: str) -> Dict[str, str]: # This is gonna assume that the the headers of the table are vertical
-    cleanedList = parseCfg(cfgFile)
-    return cleanCfg(cleanedList)
-    
-def parseCfg(cfgFile: str) -> List[str]:
     with open(cfgFile, "r") as cfgContents:
-        return [line.strip("\n") for line in cfgContents] # Removes new line characters
+        return dict([line.split("=",1) for line in [line.strip("\n") for line in cfgContents]])
 
-@cache # Using the cached operation if the list config is the same as before
-def cleanCfg(cleanedList: List[str]) -> Dict[str, str]:
-    return dict([line.split("=",1) for line in cleanedList])
+
