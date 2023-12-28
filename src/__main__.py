d2a985422794033ba5d09bc6a8a57bd54570f032
jake <C92i747A@pm.me>
2023-12-28 13:56:50 +0000

    A whole new comment :0

--- src/__main__.py
+++ src/__main__.py
@@ -85,7 +85,7 @@ def main():
 
         # Print fps
 
-        pygame.display.update()
+        pygame.display.update() # Switch this to .flip() when using GPU rendering
         # pygame.time.wait(int( (2*(1000/(FPS))) - (1000/(clock.get_fps() + 1)) )) # clock shenanigans to counter lag - DONT GET RID OF JUST YET PLS
         pygame.time.wait(int(1000 / (FPS)))  # Normal clock, * 1000 cos .wait is in ms
 
