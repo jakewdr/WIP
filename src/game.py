5539cd2256962155127128cd585685714e25a6b3
Pepsi-Man-TF2 <alexlt.ching@gmail.com>
2023-12-28 13:00:36 +0000

    split main to engine.py and game.py
    its a little messy but should be a good start, game.py also has a window class to use in future, should be good for managing deltatime too

--- src/game.py
+++ src/game.py
@@ -1,2 +1,28 @@
+import pygame
+import time
+
+
 def check():
 	print("Game window connected :p")
+
+
+class Window:
+	def __init__(self, width: int, height: int, fullscreen: int, caption: str):
+		self.window = pygame.display.set_mode((width, height), fullscreen)
+		pygame.display.set_caption(caption)
+		self.previous_time = time.time()
+
+	def swap_buffers(self):
+		pygame.display.flip()
+
+	# def draw(self, sprite: "Sprite"):
+	# self.window.blit(sprite.surface, (sprite.position.x, sprite.position.y))
+
+	def clear(self):
+		self.window.fill((0, 0, 0))
+
+	def get_dt(self):
+		current_time = time.time()
+		dt = current_time - self.previous_time
+		self.previous_time = current_time
+		return dt
