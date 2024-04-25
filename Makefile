4d0ec3dd9ea13640f77d82c369a6c75c1e1fed9a
jake <C92i747A@pm.me>
2024-04-25 12:13:10 +0100

    Updated more of the linter/format settings

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ build & bundle:
 	python bundler.py --OO
 
 format:
-	ruff check --fix src/ --config ruff.toml
-	ruff format src/ --config ruff.toml
+	ruff check --fix --no-unsafe-fixes --quiet src/ --config ruff.toml
+	ruff format --quiet src/ --config ruff.toml
 
 setup: requirements.txt
 	pip install -r requirements.txt
--- src/cfg.py
+++ src/cfg.py
@@ -1,7 +1,4 @@
-from typing import Dict
-
-
-def unpackCfg(cfgFile: str) -> Dict[str, str]:  # This is gonna assume that the the headers of the table are vertical
+def unpackCfg(cfgFile: str) -> dict[str, str]:  # This is gonna assume that the the headers of the table are vertical
     """Turns a config file into a dictionary
 
     Args:
@@ -10,6 +7,6 @@ def unpackCfg(cfgFile: str) -> Dict[str, str]:  # This is gonna assume that the 
     Returns:
         Dict[str, str]: The config file as a dictionary with the keys being the first column and the other column being the
     """
-    with open(cfgFile, "r") as cfgContents:
+    with open(cfgFile) as cfgContents:
         return dict([line.split("=", 1) for line in [line.strip("\n") for line in cfgContents if line != ""]])  # The if is needed due to a bug where the process would fail due to an empty line in the config
         # World's ugliest one liner but it gets the job done^
