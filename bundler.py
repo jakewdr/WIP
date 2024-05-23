faf08cceff02c302e15900f417b9ba44dba9f4c7
jake <C92i747A@pm.me>
2024-05-23 11:59:25 +0100

    Added multiprocessing to the bundler

--- bundler.py
+++ bundler.py
@@ -3,6 +3,7 @@ import pathlib
 import py_compile
 import shutil
 import zipfile
+from multiprocessing.pool import ThreadPool as Pool
 from time import perf_counter
 
 import python_minifier
@@ -34,20 +35,22 @@ def bundle(srcDirectory: str, outputDirectory: str, compressionLevel: int) -> No
         if ".py" in str(pathlib.Path(entry))
     ]  # If it is a verified file and is a python file
     # Below is where the compiling and optimizations happen
-    for file in pythonFiles:
-        with open(file, "r+") as fileRW:
-            minifiedCode = python_minifier.minify(fileRW.read(), rename_locals=False, rename_globals=False)  # I don't rename vars as that could cause problems when importing between files
-            fileRW.seek(0)
-            fileRW.writelines(minifiedCode)
-            fileRW.truncate()  # This line and the seek one somehow fix some corruption issues
 
-        if "__main__" not in file:  # If the __main__.py file is found in the list ignore compilation (this is to avoid the interpreter finding no entrypoint)
-            compiledFile = f"{outputDirectory}{pathLeaf(file)}c"
-            py_compile.compile(file, cfile=compiledFile, optimize=2)
-            os.remove(file)
-            compiledFiles.append(compiledFile)  # Outputs compiled python file
-        else:
-            compiledFiles.append(file)  # This is only for the __main__.py file
+    pool_size = 6
+
+    pool = Pool(pool_size)
+    for file in pythonFiles:
+        pool.apply_async(
+            compileAndMinify,
+            (
+                file,
+                outputDirectory,
+            ),
+        )
+
+    pool.close()
+    pool.join()
+
     with zipfile.ZipFile(f"{outputDirectory}bundle.py", "w", compression=zipfile.ZIP_DEFLATED, compresslevel=compressionLevel) as bundler:
         for file in compiledFiles:
             bundler.write(file, arcname=pathLeaf(file))  # pathleaf is needed to not maintain folder structure
@@ -55,6 +58,22 @@ def bundle(srcDirectory: str, outputDirectory: str, compressionLevel: int) -> No
 
     end = perf_counter()
     print(f"Bundled in {end - start} seconds")
+
+
+def compileAndMinify(file: str, outputDirectory: str):
+    with open(file, "r+") as fileRW:
+        minifiedCode = python_minifier.minify(fileRW.read(), rename_locals=False, rename_globals=False)  # I don't rename vars as that could cause problems when importing between files
+        fileRW.seek(0)
+        fileRW.writelines(minifiedCode)
+        fileRW.truncate()  # This line and the seek one somehow fix some corruption issues
+
+    if "__main__" not in file:  # If the __main__.py file is found in the list ignore compilation (this is to avoid the interpreter finding no entrypoint)
+        compiledFile = f"{outputDirectory}{pathLeaf(file)}c"
+        py_compile.compile(file, cfile=compiledFile, optimize=2)
+        os.remove(file)
+        compiledFiles.append(compiledFile)  # Outputs compiled python file
+    else:
+        compiledFiles.append(file)  # This is only for the __main__.py file
 
 
 if "__main__" in __name__:
